5382812e85902dfb3bc32b107a220cf9e07c36df
Wei Liu <weiliu@fb.com>
2017-04-08 14:36:25 -0700

    Change home image path

--- application/views/ch/index.php
+++ application/views/ch/index.php
@@ -16,16 +16,16 @@
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner">
                     <div class="item active">
-                        <img class="mvccc-slides" src="<?php echo base_url()?>/assets/img/slides/004.jpg" alt="First slide"/>
+                        <img class="mvccc-slides" src="<?php echo base_url()?>/gallery/home/001.jpg" alt="First slide"/>
                         <div class="mvccc-slides-text">歡迎來到山景城中國基督教會</div>
                         <div class="carousel-caption">First Slide</div>
                     </div>
                     <div class="item">
-                        <img class="mvccc-slides" src="<?php echo base_url()?>/assets/img/slides/005.jpg" alt="Second slide"/>
+                        <img class="mvccc-slides" src="<?php echo base_url()?>/gallery/home/002.jpg" alt="Second slide"/>
                         <div class="carousel-caption">Second Slide</div>
                     </div>
                     <div class="item">
-                        <img class="mvccc-slides" src="<?php echo base_url()?>/assets/img/slides/006.jpg" alt="Third slide"/>
+                        <img class="mvccc-slides" src="<?php echo base_url()?>/gallery/home/003.jpg" alt="Third slide"/>
                         <div class="carousel-caption">Third Slide</div>
                     </div>
                 </div>
@@ -56,7 +56,7 @@
             <p class="text-center mvccc-h4">
                 <span style="font-family:Kaiti TC">中文堂敬拜時間 禮拜日 上午 </span><span style="color: #254117">11:00</span>
             </p>
-            <p class="text-center">175 East Dana Street, Mountain View, CA 94041 | Tel (650) 964-1591 </p> 
+            <p class="text-center">175 East Dana Street, Mountain View, CA 94041 | Tel (650) 964-1591 </p>
             <hr class="mvccc-hr">
         </div>
     </div>
@@ -69,8 +69,8 @@
         <br>
         <div class="col-lg-4">
             <div class="">
-                <!-- <img class="mvccc-resource-img" src="<?php echo base_url()?>/assets/img/calendar/Calendar-icon.png" alt="Recent Events"/>  --> 
-                <h3>日歷活動</h3>      
+                <!-- <img class="mvccc-resource-img" src="<?php echo base_url()?>/assets/img/calendar/Calendar-icon.png" alt="Recent Events"/>  -->
+                <h3>日歷活動</h3>
                 <p>歡迎參加山景城中國基督教會近期活動. 請關注我們的近期消息.</p>
                 <p><a class="btn btn-default" href="#" role="button">詳細內容 &raquo;</a></p>
             </div>
@@ -96,7 +96,7 @@
 
     <hr class="mvccc-hr">
 
-    <div class="row mvccc-block-item">      
+    <div class="row mvccc-block-item">
         <br>
         <br>
         <div class="col-lg-2">
